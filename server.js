8ba90af29ee146bb072dc38b3b807a55d3954d27
travdoeswhat <123843930+vulpesviator@users.noreply.github.com>
2023-07-07 09:55:57 -0500

    Added boilerplate files for MVC & schema.sql

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
+require('dotenv').config();
+
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -14,7 +16,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'No one will ever guess',
+  secret: process.env.DB_SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
