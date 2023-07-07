8e26161dcbd939965c85f71e73db4740e2c00a92
travdoeswhat <123843930+vulpesviator@users.noreply.github.com>
2023-07-07 14:29:07 -0500

    Added login form & login route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,6 +5,13 @@ router.get('/', async (req, res) => {
   res.render('homepage');
 });
 
-
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
 
 module.exports = router;
