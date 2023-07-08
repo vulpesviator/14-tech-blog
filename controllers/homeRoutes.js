ba2635492bb3dc9ae066ac7e0c6de6395ab09a09
travdoeswhat <123843930+vulpesviator@users.noreply.github.com>
2023-07-08 11:14:07 -0500

    dashboard route & withAuth util added

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
+const { User } = require("../models");
+const withAuth = require("../utils/withauth");
 
 
 router.get('/', async (req, res) => {
   res.render('homepage');
 });
+
+router.get('/dashboard', withAuth, async (req, res) => {
+  res.render('homepage');
+})
 
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
